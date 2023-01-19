ce153b62d3c570d1127f9e5153c089792d58c758
Wisnu Strw <88566483+wsnustrw@users.noreply.github.com>
2023-01-19 13:58:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     API_KEY_RM_BG: "hL9J5m7Ps7z1qCkxmtxh26XW",
-    API_KEY_OPEN_AI: "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"
+    API_KEY_OPEN_AI: "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"
 }
